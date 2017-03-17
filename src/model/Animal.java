1173d003f8c23e4ae49747f1bbb6e20bf224d429
Mobwell <mobwell.dev@gmail.com>
2017-03-17 22:56:33 +0000

    exercicio 3

--- src/model/Animal.java
+++ src/model/Animal.java
@@ -14,6 +14,14 @@ public class Animal {
 		this.ficha = ficha;
 	}
 	
+	public double getGastosVacinas() {
+		double total = 0;
+		for ( Vacina v : ficha.getVacinas())
+			total += v.getPreco();
+		
+		return total;
+	}
+	
 	public String getClassificacao() {
 		return classificacao;
 	}
