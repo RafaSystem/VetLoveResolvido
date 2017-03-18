d3c08e93affa2072e450d4379c7aaa97a8374d0f
Rafa_ <Rafa_@DESKTOP-EF6C4CA>
2017-03-18 18:08:43 +0000

    Mensagem

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,11 @@
 package application;
 
+import controller.GestorCliente;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Ola turma!");
+		new GestorCliente();
 	}
 
 }
